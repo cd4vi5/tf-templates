8ec8d1c31fe0e7836418f0c5118159e06f9f5733
Chris Davis <cd4vi5@protonmail.com>
2023-02-24 23:00:15 +0000

    commit after tf init

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "eu-west-2"
+    access_key = "AKIAVXAQNSOUGRTYR2DD"
+    secret_key = "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"
+}
+
+resource "aws_instance" "tf-test-ec2-01" {
+ami = "ami-09ee0944866c73f62"
+instance_type = "t2.micro"
+
+tags {
+    Purpose = "Test"
+}
+
+}
+
+
