8679a9fd004a2f38fd93a4a0382b4e0eccd11a17
Chris Davis <cd4vi5@protonmail.com>
2023-02-24 23:23:33 +0000

    Destroy test ec2-1

--- main.tf
+++ main.tf
@@ -4,16 +4,3 @@ provider "aws" {
     secret_key = "ftOqu6UhKv8+vxokTJRVsAreyHDKt3I2uDDvE1vE"
 }
 
-resource "aws_instance" "tf-test-ec2-01" {
-ami = "ami-09ee0944866c73f62"
-instance_type = "t2.micro"
-
-tags = {
-    Purpose = "Test"
-    Name = "tf-test-ec2-01"
-
-}
-
-}
-
-
